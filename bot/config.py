5a03937136c80d35766c681feb4c4a8a35d1b57f
qwerty-po <qwerty@postech.ac.kr>
2025-10-06 21:49:56 +0900

    feat: load config file

--- bot/config.py
+++ bot/config.py
@@ -1,19 +1,28 @@
+from pathlib import Path
+
 import toml
 from pydantic import BaseModel
+
 
 class BotConfig(BaseModel):
     token: str
 
+
 class ConfigModel(BaseModel):
     bot: BotConfig
 
+
 class Config:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: Path):
         self.file_path = file_path
-        self.config: ConfigModel = self.load_config()
+        self.config_model: ConfigModel = self.load_config()
 
     def load_config(self) -> ConfigModel:
-        with open(self.file_path, 'r') as f:
-            data = toml.load(f)
-            return ConfigModel.model_validate_json(toml.dumps(data))
-        raise FileNotFoundError(f"Config file {self.file_path} not found.")
+        try:
+            return ConfigModel.model_validate(toml.load(self.file_path))
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Config file {self.file_path} not found.")
+        except toml.TomlDecodeError:
+            raise ValueError(f"Error decoding TOML from {self.file_path}.")
+        except Exception as e:
+            raise RuntimeError(f"An unexpected error occurred: {e}")
--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 import discord
 from app.bot import Bot
+from config import Config
 
 if __name__ == "__main__":
     intents = discord.Intents.default()
+    intents.message_content = True
+
     bot = Bot(intents=intents)
-    bot.run("YOUR_TOKEN")
+    config = Config(Path(__file__).parent / "config.toml")
+
+    bot.run(config.config_model.bot.token)
