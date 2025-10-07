b0a56939b94d91ae8640ea366ddb59f4c6963ed4
qwerty-po <qwerty@postech.ac.kr>
2025-10-07 14:08:44 +0900

    refactor: extract emoji stealing logic into a separate method

--- bot/app/bot.py
+++ bot/app/bot.py
@@ -16,19 +16,20 @@ class Bot(discord.Client):
             return
 
         if message.type == discord.MessageType.default and message.flags.forwarded:
-            async with message.channel.typing():
-                emoji_stealer = EmojiStealModel(guild=message.guild)
-                emojies = await emoji_stealer.steal_emojies_from_forwarded_message(
-                    message
-                )
+            await self._steal_emoji_from_message(message)
 
-                await message.add_reaction("<:vela_curious:1424979897520230410>")
-                if emojies:
-                    await message.reply(
-                        f"새로운 이모지들이네? 여기에도 추가해야겠어! {''.join(self._contextable_string_from_emoji(emoji) for emoji in emojies)}"
-                    )
-                else:
-                    await message.reply("이미 있는 이모지들이야?")
+    async def _steal_emoji_from_message(self, message: discord.Message):
+        async with message.channel.typing():
+            emoji_stealer = EmojiStealModel(guild=message.guild)
+            emojies = await emoji_stealer.steal_emojies_from_forwarded_message(message)
+
+            await message.add_reaction("<:vela_curious:1424979897520230410>")
+            if emojies:
+                await message.reply(
+                    f"새로운 이모지들이네? 여기에도 추가해야겠어! {''.join(self._contextable_string_from_emoji(emoji) for emoji in emojies)}"
+                )
+            else:
+                await message.reply("이미 있는 이모지들이야?")
 
     @staticmethod
     def _contextable_string_from_emoji(emoji: discord.Emoji) -> str:
